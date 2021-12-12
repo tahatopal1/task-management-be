b7842afdb142484d784b83b61767d7bb912a61ca
Taha Topal <taha.topal.7@gmail.com>
2021-12-12 05:32:02 +0300

    task creating development

--- src/main/java/com/project/taskmanagementbe/service/TaskServiceImpl.java
+++ src/main/java/com/project/taskmanagementbe/service/TaskServiceImpl.java
@@ -2,10 +2,12 @@ package com.project.taskmanagementbe.service;
 
 import com.project.taskmanagementbe.model.Task;
 import com.project.taskmanagementbe.repository.TaskRepository;
+import com.project.taskmanagementbe.repository.UsersRepository;
 import com.project.taskmanagementbe.wsdto.TaskWsDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,11 +19,20 @@ public class TaskServiceImpl implements TaskService, Converter<Task, TaskWsDto> 
     @Autowired
     private TaskRepository taskRepository;
 
+    @Autowired
+    private UsersRepository usersRepository;
+
     @Override
     public List<TaskWsDto> findAll() {
         return taskRepository.findAll().stream()
                 .map(this::convert)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    @Transactional
+    public void createTask(TaskWsDto taskWsDto, String username) {
+        taskRepository.save(Optional.ofNullable(taskWsDto).map(wsDto -> convertReverse(taskWsDto, username)).get());
     }
 
     @Override
@@ -33,4 +44,11 @@ public class TaskServiceImpl implements TaskService, Converter<Task, TaskWsDto> 
         }).ifPresent(taskWsDto::setUser);
         return taskWsDto;
     }
+
+    public Task convertReverse(TaskWsDto taskWsDto, String username){
+        Task task = new Task(taskWsDto.getId(), taskWsDto.getTitle());
+        usersRepository.findById(username).ifPresent(task::setUser);
+        return task;
+    }
+
 }
